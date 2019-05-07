007ce40f1b257e1c6d1ccd6cf4b6a7166747d556
LePaulM <p.miancien@laposte.net>
2019-05-07 15:17:23 +0200

    Add files via upload

--- ProjetEcosysteme/ecosysteme/src/Gestion/Gestionnaire.java
+++ ProjetEcosysteme/ecosysteme/src/Gestion/Gestionnaire.java
@@ -1,11 +1,20 @@
 package Gestion;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import animaux.Animal;
 
+/**
+ * Cette classe permet de gérer le timing du jeu. Elle définit les tours de jeu et crée un fichier CSV de sortie.
+ * @author Paul,Armand et Louise
+ *
+ */
 public class Gestionnaire {
 	protected static int tour = 0;
 	private static int cadence = 1000;
@@ -78,6 +87,7 @@ public class Gestionnaire {
 	 */
 	public void nouveauTour() {
 		tour = tour++;
+		List<String> ListeNomAnimaux=new ArrayList();
 		TimerTask task = new TimerTask() {
 			public void run() { 
 				for (Animal e : animaux) {								// pour chaque animal présent dans la simulation
@@ -95,12 +105,34 @@ public class Gestionnaire {
 							e.seReproduire();										// et se reproduit si il le peut
 						}
 					}
-
+					ListeNomAnimaux.add(Integer.toString(e.getId()));
 				}
 			}
 		};
+		String filename="tour_"+Integer.toString(tour);
+		writeToCsvFile(ListeNomAnimaux,";",filename);
 		Timer timer = new Timer(); timer.scheduleAtFixedRate(task, 0, cadence); 		// le tour en cours reste affiché à l'écran le temps de la cadence
+		
 	}
+
+/**
+ * Méthode permettant d'écrire un fichier CSV. Inspirée par le site : https://riptutorial.com/fr/csv/example/27605/lire-et-ecrire-en-java
+ * @param thingsToWrite : Liste des choses que l'on veut écrire dans notre fichier CSV
+ * @param separator : Le séparateur entre les différents éléments écrits dans notre fichier
+ * @param fileName : Le nom de notre fichier
+ */
+public void writeToCsvFile(List<String> thingsToWrite, String separator, String fileName){
+    try (FileWriter writer = new FileWriter(fileName)){
+        for (String strings : thingsToWrite) {
+               writer.append(strings);
+               writer.append(separator);
+               writer.append(System.lineSeparator());
+        }
+        writer.flush();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 }
 
 
